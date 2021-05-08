b5c196dfa7634b90bb1d2692f20bbe98dd80c0a9
Fabiano Brancher <fabiano.brancher@hotmail.com>
2021-05-07 21:56:03 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+const Footer = () => (
+  <footer className="footer mt-auto py-3 bg-dark">
+    <div className="container">
+      <p className="text-light">
+        App desenvolvido por{" "}
+        <a
+          href="https://github.com/fabianobrancher"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Fabiano Brancher
+        </a>
+      </p>
+      <p className="text-light">
+        <small>
+          <strong>Semana Spring React</strong>
+          <br />
+          Evento promovido pela escola DevSuperior:{" "}
+          <a
+            href="https://instagram.com/brancherfabiano"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @brancherfabiano
+          </a>
+        </small>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
